535f72ca34cf87d3719bc349d5de010510027ce1
Tanigadapa Bharath Kumar <bharathtanigadapa1@gmail.com>
2026-09-01 19:16:32 +0530

    Time: 3 ms (29.62%) | Memory: 48.6 MB (50.40%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int ans[] = new int [n+1];
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            ans[i]=hammingWeight(i);
+        }
+        return ans;
+    }
+    public int hammingWeight (int n){
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+}
