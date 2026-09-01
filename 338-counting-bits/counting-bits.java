634285d3e8eb79f99942d6424d85c168af8d64d1
Tanigadapa Bharath Kumar <bharathtanigadapa1@gmail.com>
2026-09-01 19:27:35 +0530

    Time: 2 ms (95.73%) | Memory: 48.6 MB (69.98%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -3,16 +3,9 @@ class Solution {
         int ans[] = new int [n+1];
         ans[0]=0;
         for(int i=1;i<=n;i++){
-            ans[i]=hammingWeight(i);
+            ans[i]=ans[i>>1]+(i&1); // in ehich ans[i>>1] divides the number by 2 so loosing it last most bit we need add tis to our relsult if it is one so we used i&1 which add 1 if i is odd else 0 
         }
         return ans;
     }
-    public int hammingWeight (int n){
-        int count=0;
-        while(n>0){
-            n=n&(n-1);
-            count++;
-        }
-        return count;
-    }
+   
 }
