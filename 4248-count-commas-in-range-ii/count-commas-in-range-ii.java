03ef2268ee90f3ff9b4348e1a9a6342b0e9ba939
Tanigadapa Bharath Kumar <bharathtanigadapa1@gmail.com>
2026-09-09 19:33:38 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (56.41%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public long countCommas(long n) {
+        /* The approach is finding the where the next coma is palced we know that the comma will be present after three diagts from right to left  . for a range we can say that n-newCommas number +1 results the number of commas in between the range eg: [1,000,1,000,000]  for this intially for 1000 we get 1000-1000+1 =1 commas and the next new comma  is at 1,000,000 so 1,000,000 - 1,000 +1 = 9,99,001. so result will be 9,99,002.*/
         if (n<1000) return 0;
         long ans=0;
         long nextComma=1000;
